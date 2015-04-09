7e2ac150ec2962ac8a34c3630f90b96abfb8d980
Mario Garcia <mario.ggar@gmail.com>
2015-04-09 13:59:14 +0200

    MOB: Using method coercion to refactor View.OnClickListener

--- mobile/app/src/main/groovy/shart/app/cart/CartActivity.groovy
+++ mobile/app/src/main/groovy/shart/app/cart/CartActivity.groovy
@@ -4,10 +4,10 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.app.ListActivity
 import android.content.Intent
-import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.arasthel.swissknife.annotations.OnBackground
 import com.arasthel.swissknife.annotations.OnUIThread
 import groovy.transform.CompileStatic
 import shart.app.R
@@ -28,20 +28,20 @@ class CartActivity extends ListActivity {
         final Activity thisActivity = this
 
         Button button = (Button) findViewById(R.id.button_add_new_cart)
-        button.setOnClickListener { View v ->
-            def task =  [
-                doInBackground : { Object[] params ->
-                     return from(thisActivity).carts.createNewCart(new Cart(cartDate: toRequest(new Date())))
-                },
-                onPostExecute : { Cart cart ->
-                    Intent intent = new Intent(thisActivity, CartItemListActivity)
-                    intent.putExtra('cart', cart)
-                    thisActivity.startActivity(intent)
-                }
-            ] as AsyncTask<Object,Integer,Cart>
+        button.setOnClickListener(this.&createNewCart)
+    }
 
-            task.execute()
-        }
+    @OnBackground
+    void createNewCart(View view) {
+        Cart cart =
+            from(this)
+            .carts
+            .createNewCart(new Cart(cartDate: toRequest(new Date())))
+
+        Intent intent = new Intent(this, CartItemListActivity)
+        intent.putExtra('cart', cart)
+
+        startActivity(intent)
     }
 
     @OnUIThread
